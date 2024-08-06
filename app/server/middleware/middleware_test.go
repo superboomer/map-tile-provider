edd29538bb3dec81801323368d1107b75a87553e
Ivan <2558124+superboomer@users.noreply.github.com>
2024-08-07 00:53:03 +0300

    for what??

--- app/server/middleware/middleware_test.go
+++ app/server/middleware/middleware_test.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 
 	"go.uber.org/zap"
 
@@ -25,6 +26,58 @@ func TestMiddleware_Log(t *testing.T) {
 
 	assert.Equal(t, http.StatusOK, rr.Code)
 	assert.Contains(t, rr.Body.String(), "request completed")
+}
+
+func TestMiddleware_LogDurationsLong(t *testing.T) {
+	md := &MD{
+		Logger: zap.NewNop(),
+	}
+
+	handler := md.Log(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(time.Second * 1)
+		_, _ = w.Write([]byte("request completed"))
+	}))
+	req, _ := http.NewRequest("GET", "/", http.NoBody)
+	rr := httptest.NewRecorder()
+
+	handler.ServeHTTP(rr, req)
+
+	assert.Equal(t, http.StatusOK, rr.Code)
+	assert.Contains(t, rr.Body.String(), "request completed")
+}
+
+func TestMiddleware_LogDurationsMedium(t *testing.T) {
+	md := &MD{
+		Logger: zap.NewNop(),
+	}
+
+	handler := md.Log(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(time.Millisecond * 100)
+		_, _ = w.Write([]byte("request completed"))
+	}))
+	req, _ := http.NewRequest("GET", "/", http.NoBody)
+	rr := httptest.NewRecorder()
+
+	handler.ServeHTTP(rr, req)
+
+	assert.Equal(t, http.StatusOK, rr.Code)
+	assert.Contains(t, rr.Body.String(), "request completed")
+}
+
+func TestMiddleware_LogDurationsMinimum(t *testing.T) {
+	md := &MD{
+		Logger: zap.NewNop(),
+	}
+
+	handler := md.Log(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+	}))
+	req, _ := http.NewRequest("GET", "/", http.NoBody)
+	rr := httptest.NewRecorder()
+
+	handler.ServeHTTP(rr, req)
+
+	assert.Equal(t, http.StatusOK, rr.Code)
 }
 
 func TestMiddleware_RequestID(t *testing.T) {
