cb0d50fa3d27b1abf5bd0e638f96abd1098f8913
Ivan <2558124+superboomer@users.noreply.github.com>
2024-08-06 20:45:10 +0300

    fix TestRun

--- app/server/run_test.go
+++ app/server/run_test.go
@@ -28,20 +28,20 @@ func TestRunHTTP(t *testing.T) {
 	}
 }
 
-// // Test Run
-// func TestRun(t *testing.T) {
-// 	logger := zap.NewNop()
-// 	opts := &options.Opts{APIPort: "8080", Swagger: true, Schema: "https://raw.githubusercontent.com/superboomer/map-tile-provider/master/example/providers.json"}
+// Test Run
+func TestRun(t *testing.T) {
+	logger := zap.NewNop()
+	opts := &options.Opts{APIPort: "8080", Swagger: true, Schema: "./../../example/providers.json"}
 
-// 	ctx, cancel := context.WithCancel(context.Background())
+	ctx, cancel := context.WithCancel(context.Background())
 
-// 	go func() {
-// 		time.Sleep(100 * time.Millisecond) // Allow server to start
-// 		cancel()                           // Cancel the context to trigger shutdown
-// 	}()
+	go func() {
+		time.Sleep(100 * time.Millisecond) // Allow server to start
+		cancel()                           // Cancel the context to trigger shutdown
+	}()
 
-// 	err := Run(ctx, logger, opts)
-// 	if err != nil {
-// 		t.Fatalf("expected no error, got %v", err)
-// 	}
-// }
+	err := Run(ctx, logger, opts)
+	if err != nil {
+		t.Fatalf("expected no error, got %v", err)
+	}
+}
