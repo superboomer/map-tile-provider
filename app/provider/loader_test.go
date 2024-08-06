d61dee436f31b7a59540a49908facfd61e52037e
Ivan <2558124+superboomer@users.noreply.github.com>
2024-08-06 22:44:30 +0300

    added tests for loader.go

---
+++ app/provider/loader_test.go
@@ -0,0 +1,50 @@
+package provider_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/superboomer/map-tile-provider/app/provider"
+)
+
+// Assuming the existence of a generated mock for the List interface named ListMock
+type MockList struct {
+	provider.ListMock
+}
+
+func TestLoadProviderList_Success(t *testing.T) {
+	list, err := provider.LoadProviderList("./../../example/providers.json")
+
+	// Assert
+	assert.NoError(t, err)
+	assert.NotNil(t, list)
+}
+
+func TestLoadProviderList_ErrorLoadingJSON(t *testing.T) {
+	list, err := provider.LoadProviderList("invalid/path")
+
+	// Assert
+	assert.Error(t, err)
+	assert.Contains(t, err.Error(), "error occurred when loading providers schema")
+	assert.Nil(t, list)
+}
+
+func TestLoadProviderList_ErrorCreatingProvider(t *testing.T) {
+	// Execute
+	list, err := provider.LoadProviderList("./testdata/providers_invalid.json")
+
+	// Assert
+	assert.Error(t, err)
+	assert.Contains(t, err.Error(), "projection unknown not found for provider")
+	assert.Nil(t, list)
+}
+
+func TestLoadProviderList_ErrorRegisteringProvider(t *testing.T) {
+	// Execute
+	list, err := provider.LoadProviderList("./testdata/providers_duplicate.json")
+
+	// Assert
+	assert.Error(t, err)
+	assert.Contains(t, err.Error(), "error occurred when registering new provider")
+	assert.Nil(t, list)
+}
